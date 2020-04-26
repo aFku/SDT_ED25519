9797da9e091f31a8dc19b71849893189920b7a7e
aFku <44970719+aFku@users.noreply.github.com>
2020-04-26 16:19:20 +0200

    new

--- main.py
+++ main.py
@@ -1,170 +1,73 @@
 import hashlib
+import os
 import binascii
-
-def sha512(s):
-    return hashlib.sha512(s).digest()
-
-# Base field Z_p
-p = 2**255 - 19
-
-def modp_inv(x):
-    return pow(x, p-2, p)
-
-# Curve constant
-d = -121665 * modp_inv(121666) % p
-
-# Group order
-q = 2**252 + 27742317777372353535851937790883648493
-
-def sha512_modq(s):
-    return int.from_bytes(sha512(s), "little") % q
-
-## Then follows functions to perform point operations.
-
-# Points are represented as tuples (X, Y, Z, T) of extended
-# coordinates, with x = X/Z, y = Y/Z, x*y = T/Z
-
-def point_add(P, Q):
-    A, B = (P[1]-P[0]) * (Q[1]-Q[0]) % p, (P[1]+P[0]) * (Q[1]+Q[0]) % p;
-    C, D = 2 * P[3] * Q[3] * d % p, 2 * P[2] * Q[2] % p;
-    E, F, G, H = B-A, D-C, D+C, B+A;
-    return (E*F, G*H, F*G, E*H);
-
-# Computes Q = s * Q
-def point_mul(s, P):
-    Q = (0, 1, 1, 0)  # Neutral element
-    while s > 0:
-        if s & 1:
-            Q = point_add(Q, P)
-        P = point_add(P, P)
-        s >>= 1
-    return Q
-
-def point_equal(P, Q):
-    # x1 / z1 == x2 / z2  <==>  x1 * z2 == x2 * z1
-    if (P[0] * Q[2] - Q[0] * P[2]) % p != 0:
-        return False
-    if (P[1] * Q[2] - Q[1] * P[2]) % p != 0:
-        return False
-    return True
-
-## Now follows functions for point compression.
-
-# Square root of -1
-modp_sqrt_m1 = pow(2, (p-1) // 4, p)
-
-# Compute corresponding x-coordinate, with low bit corresponding to
-# sign, or return None on failure
-def recover_x(y, sign):
-    if y >= p:
-        return None
-    x2 = (y*y-1) * modp_inv(d*y*y+1)
-    if x2 == 0:
-        if sign:
-            return None
-        else:
-            return 0
-
-    # Compute square root of x2
-    x = pow(x2, (p+3) // 8, p)
-    if (x*x - x2) % p != 0:
-        x = x * modp_sqrt_m1 % p
-    if (x*x - x2) % p != 0:
-        return None
-
-    if (x & 1) != sign:
-        x = p - x
-    return x
-
-# Base point
-g_y = 4 * modp_inv(5) % p
-g_x = recover_x(g_y, 0)
-G = (g_x, g_y, 1, g_x * g_y % p)
-
-def point_compress(P):
-    zinv = modp_inv(P[2])
-    x = P[0] * zinv % p
-    y = P[1] * zinv % p
-    return int.to_bytes(y | ((x & 1) << 255), 32, "little")
-
-def point_decompress(s):
-    if len(s) != 32:
-        raise Exception("Invalid input length for decompression")
-    y = int.from_bytes(s, "little")
-    sign = y >> 255
-    y &= (1 << 255) - 1
-
-    x = recover_x(y, sign)
-    if x is None:
-        return None
-    else:
-        return (x, y, 1, x*y % p)
-
-## These are functions for manipulating the private key.
-
-def secret_expand(secret):
-    if len(secret) != 32:
-        raise Exception("Bad size of private key")
-    h = sha512(secret)
-    a = int.from_bytes(h[:32], "little")
-    a &= (1 << 254) - 8
-    a |= (1 << 254)
-    return (a, h[32:])
-
-def secret_to_public(secret):
-    (a, dummy) = secret_expand(secret)
-    return point_compress(point_mul(a, G))
-
-## The signature function works as below.
-
-def sign(secret, msg):
-    a, prefix = secret_expand(secret)
-    A = point_compress(point_mul(a, G))
-    r = sha512_modq(prefix + msg)
-    R = point_mul(r, G)
-    Rs = point_compress(R)
-    h = sha512_modq(Rs + A + msg)
-    s = (r + h * a) % q
-    return Rs + int.to_bytes(s, 32, "little")
-
-## And finally the verification function.
-
-def verify(public, msg, signature):
-    if len(public) != 32:
-        raise Exception("Bad public key length")
-    if len(signature) != 64:
-        Exception("Bad signature length")
-    A = point_decompress(public)
-    if not A:
-        return False
-    Rs = signature[:32]
-    R = point_decompress(Rs)
-    if not R:
-        return False
-    s = int.from_bytes(signature[32:], "little")
-    if s >= q: return False
-    h = sha512_modq(Rs + public + msg)
-    sB = point_mul(s, G)
-    hA = point_mul(h, A)
-    return point_equal(sB, point_add(R, hA))
-
-def munge_string(s, pos, change):
-    return (s[:pos] +
-            int.to_bytes(s[pos] ^ change, 1, "little") +
-            s[pos+1:])
-
-def str_to_bitlist(string: str):
-    return [format(ord(i), 'b') for i in string]
 
 def change_string_with_int(string: str, index, number: int):
     tmp = list(string)
     tmp[index] = hex(number)[-1]
     return ''.join(tmp)
 
-if __name__ == "__main__":
+p = pow(2, 448) - pow(2, 224) - 1
 
-    y = 'abcdf15'
-    print(bytes(y.encode())
+base = 224580040295924300187604334099896036246789641632564134246125461686950415467406032909029192869357953282578032075146446173674602635247710, 298819210078481492676017930443930673437544040154080242095928241372331506189835876003536878655418784733982303233503462500531545062832660
+
+Private_key_bytes = 57
+Public_key_bytes = Private_key_bytes
+Signature_bytes = Private_key_bytes + Public_key_bytes
+
+# Secret key is random
+Private_key = binascii.b2a_hex(os.urandom(8*Private_key_bytes))
+print("Private key:", Private_key)
+# Public key generation
+Public1 = hashlib.shake_256()
+Public1.update(Private_key)
+print("Hashed:     ", Public1.hexdigest(114))
+Public2 = Public1.hexdigest(114)
+Public3 = Public2[-114:]
+print("Lower 57:   ", Public3)
+########################################################################################
+Public4 = []
+for index in range(1, len(Public3), 2):
+    num1 = int(Public3[index-1], 16)
+    num2 = int(Public3[index], 16)
+    num3 = num1 << 4 | num2
+    if len(str(hex(num3))[2:]) < 2:
+        Public4.append('0' + str(hex(num3))[2:])
+    else:
+        Public4.append(str(hex(num3))[2:])
+
+tmp = str(hex(int(Public4[0], 16) & 0b11111100))
+if len(tmp) < 4:
+    tmp = '0' + tmp[2:]
+else:
+    tmp = tmp[2:]
+Public4[0] = tmp
+
+for index, octet in enumerate(Public4[1:]):
+        tmp = str(hex(int(octet, 16) | 0b10000000))
+        if len(tmp) < 4:
+            tmp = '0' + tmp[2:]
+        else:
+            tmp = tmp[2:]
+
+        Public4[index+1] = tmp
+
+
+
+print(Public4)
+x = bytearray()
+for q in Public4:
+    tmp = int(q, 16)
+    x.append(tmp)
+
+print(x)
+print(int.from_bytes(x, 'little'))
+
+#Public3 = binascii.unhexlify(Public3)
+#a = int.from_bytes(Public3[:57], "little")
+#print(a)
+
+
 
 
 
--- sdt_448.py
+++ sdt_448.py
@@ -1,13 +1,6 @@
-# SDT Project, ed448
-
 import hashlib
 import os
 import binascii
-
-def change_string_with_int(string: str, index, number: int):
-    tmp = list(string)
-    tmp[index] = hex(number)[-1]
-    return ''.join(tmp)
 
 p = pow(2, 448) - pow(2, 224) - 1
 
@@ -18,7 +11,7 @@ Public_key_bytes = Private_key_bytes
 Signature_bytes = Private_key_bytes + Public_key_bytes
 
 # Secret key is random
-Private_key = binascii.b2a_hex(os.urandom(Private_key_bytes))
+Private_key = binascii.b2a_hex(os.urandom(8*Private_key_bytes))
 print("Private key:", Private_key)
 # Public key generation
 Public1 = hashlib.shake_256()
@@ -27,40 +20,44 @@ print("Hashed:     ", Public1.hexdigest(114))
 Public2 = Public1.hexdigest(114)
 Public3 = Public2[-114:]
 print("Lower 57:   ", Public3)
-########################################################################################
 
-Public4 = int(Public3[0], 16)
-Public4 = Public4 & 1100 #  The two least significant bits of the first octet are cleared
-Public3 = change_string_with_int(Public3, 0, Public4)
+Public4 = []
 
-########################################################################################
-########################################################################################
+for index in range(1, len(Public3), 2):
+    num1 = int(Public3[index-1], 16)
+    num2 = int(Public3[index], 16)
+    num3 = num1 << 4 | num2
+    if len(str(hex(num3))[2:]) < 2:
+        Public4.append('0' + str(hex(num3))[2:])
+    else:
+        Public4.append(str(hex(num3))[2:])
 
-Public5 = int(Public3[-1], 16)
-Public5 = Public5 & 0000 # All eight bits the last octet are cleared
-Public3 = change_string_with_int(Public3, -1, Public5)
-########################################################################################
-########################################################################################
+tmp = str(hex(int(Public4[0], 16) & 0b11111100))
 
-for x in range(1, len(Public3)):
-    tmp = int(Public3[x], 16)
-    tmp = tmp | 1000
-    Public3 = change_string_with_int(Public3, x, tmp) # the highest bit of the second to last octet is set
+if len(tmp) < 4:
+    tmp = '0' + tmp[2:]
+else:
+    tmp = tmp[2:]
+Public4[0] = tmp
 
-########################################################################################
-########################################################################################
+tmp = str(hex(int(Public4[-1], 16) & 0b00000000))
+Public4[-1] = tmp
 
+for index, octet in enumerate(Public4[1:]):
+    tmp = str(hex(int(octet, 16) | 0b10000000))
+    if len(tmp) < 4:
+        tmp = '0' + tmp[2:]
+    else:
+        tmp = tmp[2:]
 
-
-#a = int.from_bytes(Public3[:114].encode(), "little")
-print(Public3)
-Public3 = binascii.unhexlify(Public3)
-a = int.from_bytes(Public3[:57], "little")
-print(a)
-#print(sys.getsizeof(Public3.encode()))
-
-
+    Public4[index + 1] = tmp
 
 
+print(Public4)
+x = bytearray()
+for q in Public4:
+    tmp = int(q, 16)
+    x.append(tmp)
 
-
+print(x)
+print(int.from_bytes(x, 'little'))
